081513df4419a5a78967a0ee8b9ea842bd4d1af5
jonas <jonas@jonas-Inspiron-5579>
2018-04-30 14:32:09 +0200

    ...

--- src/main/java/de/asc/db/Database.java
+++ src/main/java/de/asc/db/Database.java
@@ -1,19 +1,35 @@
-/**
- * 
- */
 package de.asc.db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- * @author andi
- *
- */
 public class Database {
 
-	/**
-	 * 
-	 */
-	public Database() {
-		// TODO Auto-generated constructor stub
-	}
+    public static void main(String[] args) {
 
+        String url = "jdbc:postgresql://localhost:5432/timetracker";
+        String user = "time";
+        String password = "jonas";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM users")) {
+
+            while (rs.next()) {
+                System.out.println(rs.getString(1));
+                System.out.println(rs.getString(2));
+               
+                //System.out.println(rs.getString(6));
+            }
+
+        } catch (SQLException ex) {
+        
+            Logger lgr = Logger.getLogger(Connect.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
 }
